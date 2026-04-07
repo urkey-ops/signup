4f070dafa34a2bc0988d3b276b1a4acb8d6a45bd
urkey-ops <keyurdudakiya@gmail.com>
2026-04-07 15:04:34 -0400

    Update admin-script.js

--- admin-script.js
+++ admin-script.js
@@ -6,10 +6,9 @@
 const API_URL = '/api/admin';
 
 const DEFAULT_SLOTS = [
-    { label: "10AM - 12PM", capacity: 6 },
-    { label: "12PM - 2PM", capacity: 6 },
-    { label: "2PM - 4PM", capacity: 6 },
-    { label: "4PM - 6PM", capacity: 6 },
+  { label: '9AM - 12PM',  capacity: 6 },
+  { label: '12PM - 3PM',  capacity: 6 },
+  { label: '3PM - 6PM',   capacity: 6 },
 ];
 
 let loadedSlots = [];
